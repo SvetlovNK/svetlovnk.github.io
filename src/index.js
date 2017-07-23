e1d1731c10b2ff9abb8476a10c75f2b176ee17a8
Nickolay Svetlov <ns@idaproject.com>
2017-07-23 15:35:50 +0300

    fix 'forEach' of null

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { save, load } from './helpers';
 const state = load();
 
 const model = new Model(state);
-model.on('change', state => save(state));
+model.on('change', state => save(state || undefined));
 
 const view = new View();
 const controller = new Controller(model, view);
